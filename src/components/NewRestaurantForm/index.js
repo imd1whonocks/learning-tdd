e35d2040e2a64b887d1bcb7705ec1c137f23432c
Shashank Kumar Singh <shashankkumar.singh@jungleworks.com>
2020-04-26 16:01:11 +0530

    UI Refactor

--- src/components/NewRestaurantForm/index.js
+++ src/components/NewRestaurantForm/index.js
@@ -3,17 +3,28 @@ import {connect} from 'react-redux';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Alert from '@material-ui/lab/Alert';
+import Box from '@material-ui/core/Box';
+import {makeStyles} from '@material-ui/core/styles';
 
 import {createRestaurant} from '../../store/restaurants/actions';
 
+const useStyles = makeStyles(theme => ({
+	root: {
+		'& > *': {
+			margin: theme.spacing(1),
+		},
+	},
+}));
+
 export const NewRestaurantForm = ({createRestaurant}) => {
+	const classes = useStyles();
 	const handleSubmit = e => {
 		e.preventDefault();
 		if (name === '') {
 			setShowValidationError(true);
 		} else {
-            setShowValidationError(false);
-            setServerError(false);
+			setShowValidationError(false);
+			setServerError(false);
 			createRestaurant(name)
 				.then(() => setName(''))
 				.catch(() => setServerError(true));
@@ -30,21 +41,23 @@ export const NewRestaurantForm = ({createRestaurant}) => {
 				</Alert>
 			)}
 			{showValidationError && <Alert severity="error">Name is required</Alert>}
-			<TextField
-				value={name}
-				onChange={e => setName(e.target.value)}
-				placeholder="Add Restaurant"
-				fullWidth
-				variant="filled"
-			/>
-			<Button
-				type="submit"
-				variant="contained"
-				color="primary"
-				data-testid="new-restaurant-submit-button"
-			>
-				Add
-			</Button>
+			<Box display="flex" className={classes.root}>
+				<TextField
+					value={name}
+					onChange={e => setName(e.target.value)}
+					placeholder="Add Restaurant"
+					fullWidth
+					variant="filled"
+				/>
+				<Button
+					type="submit"
+					variant="contained"
+					color="primary"
+					data-testid="new-restaurant-submit-button"
+				>
+					Add
+				</Button>
+			</Box>
 		</form>
 	);
 };
